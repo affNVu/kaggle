3cfe7420830d958def0eca3c6e09fe6e8fd661be
nvu <vtngoc90@gmail.com>
2024-12-31 10:11:05 +0000

    update

---
+++ JS-RMF/incremental.py
@@ -0,0 +1,125 @@
+import polars as pl
+from sklearn.model_selection._split import _BaseKFold, indexable, _num_samples
+import numpy as np
+
+input_path = 'inputs/train.parquet/*/*.parquet'
+lf = pl.scan_parquet(input_path)
+#
+columns = lf.columns
+features_cols = [x for x in columns if 'feature' in x]
+responder_cols = [x for x in columns if 'responder' in x]
+target_col = 'responder_6'
+# dates 
+dates = sorted(lf.select(['date_id']).unique().collect()['date_id'].to_list())
+time_steps = sorted(lf.select(['time_id']).unique().collect()['time_id'].to_list())
+
+class PurgedGroupTimeSeriesSplit:
+    def __init__(
+        self,
+        train_days: int,
+        test_days: int,
+        gap: int = 0,
+        max_n_splits: int = None,
+        min_train_days: int = 1,
+    ):
+        self.train_days = train_days
+        self.gap = gap
+        self.test_days = test_days
+        self.max_n_splits = max_n_splits
+        self.min_train_days = min_train_days
+        if train_days <= 0 or test_days <= 0 or min_train_days <= 0 or gap < 0:
+            raise ValueError("Non positive input for days")
+        if max_n_splits is not None and max_n_splits <= 0:
+            raise ValueError("Non positive input for max_n_splits")
+
+    def split(self, X, y):
+        if not all(y.index == X.index):
+            raise ValueError(
+                "Indexes of feature data and response data are not the same"
+            )
+
+        X, y = indexable(X, y)
+        test_days = self.test_days
+        gap = self.gap
+        train_days = self.train_days
+        min_train_days = self.min_train_days
+        max_n_splits = self.max_n_splits
+        unique_groups = sorted(list(y.reset_index()["date_id"].unique()))
+        n_groups = _num_samples(unique_groups)
+        if min_train_days + gap + test_days > n_groups:
+            raise ValueError(
+                "Number of days in data is less than the minimum days need for a fold"
+            )
+
+        group_test_starts = list(range(n_groups - test_days, 0, -test_days))
+        if max_n_splits is not None:
+            group_test_starts = group_test_starts[:max_n_splits]
+        group_test_starts = [
+            g for g in group_test_starts if g - gap - min_train_days >= 0
+        ]
+
+        for group_test_start in group_test_starts:
+            group_train_end = group_test_start - gap
+            group_train_start = max(group_train_end - train_days, 0)
+            train_dates = unique_groups[group_train_start:group_train_end]
+            test_dates = unique_groups[group_test_start : group_test_start + test_days]
+            test_index_array = X[X['date_id'].isin(test_dates)].index
+            train_index_array = X[X['date_id'].isin(train_dates)].index
+            X_test, y_test = X.iloc[test_index_array], y.iloc[test_index_array]
+            X_train, y_train = X.iloc[train_index_array], y.iloc[train_index_array]
+            out = [X_train, X_test, y_train, y_test]
+            out = [x.drop(columns='date_id') for x in out]
+            yield out
+
+
+def reduce_mem_usage(df):
+    """ iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.        
+    """
+    start_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+    
+    for col in df.columns:
+        col_type = df[col].dtype
+        
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+        else:
+            df[col] = df[col].astype('category')
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+    
+    return df
+
+def get_dataset_by_date_id(start: int, end: int, cols: list[str] = features_cols + [target_col]):
+    lf = pl.scan_parquet(input_path)
+    filter = ((pl.col('date_id') >= start) & (pl.col('date_id') <= end))
+    df = lf.filter(filter).select(cols).collect()
+    return reduce_mem_usage(df.to_pandas())
+
+# 
+num_dates_per_training = 100
+
+def foo(
+    num_dates: int
+):
+    
--- JS-RMF/pytorch.py
+++ JS-RMF/pytorch.py
@@ -1,3 +1,4 @@
+# import xgboost as xgb
 import torch
 import torch.nn as nn
 import numpy as np
@@ -9,8 +10,10 @@ import numpy as np
 from sklearn import preprocessing, linear_model, model_selection, pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.svm import SVR
+from sklearn.linear_model import Ridge
+from sklearn.neural_network import MLPRegressor
 import math
 
 def reduce_mem_usage(df):
@@ -50,6 +53,22 @@ def reduce_mem_usage(df):
     print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
     
     return df
+
+
+def make_prediction(preproc, dae, reg, X):
+    with torch.no_grad():
+        X_trf = preproc.transform(X)
+        X_trf = torch.from_numpy(X_trf)
+        X_embeded = dae.encode(X_trf)
+        y_pred = reg(X_embeded)
+    return y_pred
+
+
+def score_predictions(preproc, dae, reg, X, y):
+    y_pred = make_prediction(preproc=preproc, dae=dae, reg=reg, X=X)
+    y_pred = y_pred.numpy()
+    y = y.numpy()
+    return r2_score(y_pred=y_pred, y_true=y)
 
 
 def find_highest_gradient(model):
@@ -93,21 +112,89 @@ def evaluate_model(model, train_loader, val_loader, device, eval_iter, loss_fn):
     return train_loss, val_loss
 
 
+def train_model(model, train_loader, test_loader, optimizer, device,
+    n_epochs, eval_freq, eval_iter, loss_fn,
+    warmup_steps, early_stopper, initial_lr=3e-05, min_lr=1e-6):
+    train_losses, val_losses, track_tokens_seen, track_lrs = [], [], [], []
+    global_step = -1
+
+    # Retrieve the maximum learning rate from the optimizer
+    peak_lr = optimizer.param_groups[0]["lr"]
+
+    # Calculate the total number of iterations in the training process
+    total_training_steps = len(train_loader) * n_epochs
+
+    # Calculate the learning rate increment during the warmup phase
+    lr_increment = (peak_lr - initial_lr) / warmup_steps
+
+
+    for epoch in range(n_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            global_step += 1
+
+            # Adjust the learning rate based on the current phase (warmup or cosine annealing)
+            if global_step < warmup_steps:
+                # Linear warmup
+                lr = initial_lr + global_step * lr_increment  
+            else:
+                # Cosine annealing after warmup
+                progress = ((global_step - warmup_steps) / 
+                            (total_training_steps - warmup_steps))
+                lr = min_lr + (peak_lr - min_lr) * 0.5 * (1 + math.cos(math.pi * progress))
+
+            # Apply the calculated learning rate to the optimizer
+            for param_group in optimizer.param_groups:
+                param_group["lr"] = lr
+            track_lrs.append(optimizer.param_groups[0]["lr"])
+
+                # Calculate and backpropagate the loss
+
+            loss = calc_loss_batch(input_batch, target_batch, model=model, loss_fn=loss_fn, device='cpu')
+            loss.backward()
+
+            # Apply gradient clipping after the warmup phase to avoid exploding gradients
+
+            if global_step > warmup_steps:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)  
+                
+            optimizer.step()
+            # Periodically evaluate the model on the training and validation sets
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, test_loader,
+                    'cpu', 1, loss_fn
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                # Print the current losses
+                print(f"Ep {epoch+1} (Iter {global_step:06d}): "
+                        f"Train loss {train_loss:.3f}, "
+                        f"Val loss {val_loss:.3f}"
+                )
+    
+
 class DAE(nn.Module):
     def __init__(self, d: int):
         super().__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(d, 32),
+            nn.Linear(d, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
             nn.ReLU(),
             nn.Linear(32, 16),
             nn.ReLU(),
+            nn.Linear(16, 8)
         ).double()
         self.decoder = nn.Sequential(
+            nn.Linear(8, 16),
+            nn.ReLU(),
             nn.Linear(16, 32),
             nn.ReLU(),
-            nn.Linear(32, d),
+            nn.Linear(32, 64),
             nn.ReLU(),
-            nn.Identity(d)
+            nn.Linear(64, d),
         ).double()
 
     def encode(self, x):
@@ -132,39 +219,24 @@ class Regressor(nn.Module):
     def forward(self, x):
         return self.reg(x)
 
-# class JSDataset(IterableDataset):
-#     def __init__(self, path: str, start: float, end: float, batch_size: int):
-#         super().__init__()
-#         self.path = path
-#         self.batch_size = batch_size
-#         lf = pl.scan_parquet(self.path)
-#         # get cols
-#         cols = lf.columns
-#         self.feature_cols = [x for x in cols if 'feature' in x]
-#         self.target_col = 'responder_6'
-#         # resolve indicies
-#         n = lf.select(pl.len()).collect().item()
-#         self.start_idx = int(n * start)
-#         self.end_idx = int(n * end)
-#         n = self.end_idx - self.start_idx + 1
-#         self.num_batch = n // batch_size
-#         self.n = n
-#         self.current_idx = 0
-#         print(f'n: {self.n}, start_idx: {self.start_idx}, end_idx: {self.end_idx}, num_batch: {self.num_batch}')
+class EarlyStopper:
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float('inf')
 
-#     def __iter__(self):
-#         lf = pl.scan_parquet(self.path)
-#         rows = list(range(self.batch_size * self.current_idx, self.batch_size * (self.current_idx +1)))
-#         df = lf.select(pl.all().gather(rows)).collect(streaming=True)
-#         _X = df[self.feature_cols].to_numpy()
-#         noise = np.random.normal(0,1,X.shape)
-#         X = _X + noise
-#         y = _X
-#         # move idx
-#         self.current_idx = (self.current_idx + 1) % self.num_batch
-#         yield X, y
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
 
-lf = pl.scan_parquet('inputs/train.parquet/*/*.parquet').head(int(1e6))
+lf = pl.scan_parquet('inputs/train.parquet/*/*.parquet').head(int(3e6))
 cols = lf.columns
 feature_cols = [x for x in cols if 'feature' in x]
 target_col = 'responder_6'
@@ -176,137 +248,193 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_s
 preproc = pipeline.Pipeline(
     steps=[
         ('impute', SimpleImputer(strategy='mean', fill_value=0)),
-        ('min_max', preprocessing.MinMaxScaler()),
-        ('norm', preprocessing.StandardScaler()),
+        # ('min_max', preprocessing.MinMaxScaler()),
+        # ('norm', preprocessing.StandardScaler()),
     ]
 )
 X_train_trf = preproc.fit_transform(X_train)
+X_test_trf = preproc.transform(X_test)
 noise = np.random.normal(0,1,X_train_trf.shape)
 X_train_trf_corrupted = X_train_trf + noise
 
+# base models
 
+# pred_head = 10 ** 5
+# xgb_reg = xgb.XGBRegressor()
+# xgb_reg.fit(X_train_trf, y_train)
+# y_pred_xgb = xgb_reg.predict(X_test_trf[:pred_head])
+# r2_xgb = r2_score(y_true=y_test[:pred_head],y_pred=y_pred_xgb)
+# print(f'Base Linee XGB: {r2_xgb}')
 
+# # ridge
+# ridge = Ridge().fit(X_train_trf, y_train)
+# y_pred_ridge = ridge.predict(X_test_trf[:pred_head])
+# r2_ridge = r2_score(y_true=y_test[:pred_head],y_pred=y_pred_ridge)
+# print(f'Base Linee Ridge: {r2_ridge}')
+
+# # mlp
+# mlp = MLPRegressor().fit(X_train_trf, y_train)
+# y_pred_mlp = mlp.predict(X_test_trf[:pred_head])
+# r2_mlp = r2_score(y_true=y_test[:pred_head],y_pred=y_pred_mlp)
+# print(f'Base Linee MLP: {r2_mlp}')
+
+# training DAE
 # Create dataloader
-batch_size=2**10
+# batch_size=2**10
+n_epochs = 100
 shuffle=True
 drop_last=True
 num_workers=0
-train_ratio = 0.90
-split_idx = int(train_ratio * len(X_train_trf))
+# train_ratio = 0.90
+# split_idx = int(train_ratio * len(X_train_trf))
 
-train_loader = DataLoader(
-    list(zip(X_train_trf_corrupted[:split_idx], X_train_trf[:split_idx])),
-    batch_size=batch_size,
-    shuffle=shuffle,
-    drop_last=drop_last,
-    num_workers=num_workers
-)
-test_loader = DataLoader(
-    list(zip(X_train_trf_corrupted[split_idx:], X_train_trf[split_idx:])),
-    batch_size=batch_size,
-    shuffle=shuffle,
-    drop_last=drop_last,
-    num_workers=num_workers
-)
+# train_loader = DataLoader(
+#     list(zip(X_train_trf_corrupted[:split_idx], X_train_trf[:split_idx])),
+#     batch_size=batch_size,
+#     shuffle=shuffle,
+#     drop_last=drop_last,
+#     num_workers=num_workers
+# )
+# test_loader = DataLoader(
+#     list(zip(X_train_trf_corrupted[split_idx:], X_train_trf[split_idx:])),
+#     batch_size=batch_size,
+#     shuffle=shuffle,
+#     drop_last=drop_last,
+#     num_workers=num_workers
+# )
 
-initial_lr = 0.0001
-peak_lr = 0.01
-n_epochs = 100
-total_training_steps = len(train_loader) * n_epochs
-
-warmup_steps = int(0.2 * total_training_steps) # 20% warmup
-print(warmup_steps)
-loss_fn = nn.MSELoss()
-dae = DAE(d=X_train_trf.shape[1])
-learning_rate = 1e-4
-weight_decay = 1e-2
-optimizer = torch.optim.AdamW(dae.parameters(), weight_decay=0.1)
-lr_increment = (peak_lr - initial_lr) / warmup_steps
-global_step = -1
-dae.train()
-track_lrs = []
-min_lr = 0.1 * initial_lr
-loss_fn = nn.MSELoss()
-eval_freq = 10
-train_losses = []
-val_losses = []
-for epoch in range(n_epochs):
-    dae.train()
-    for input_batch, target_batch in train_loader:
-        optimizer.zero_grad()
-        global_step += 1
-
-        # Adjust the learning rate based on the current phase (warmup or cosine annealing)
-        if global_step < warmup_steps:
-            # Linear warmup
-            lr = initial_lr + global_step * lr_increment  
-        else:
-            # Cosine annealing after warmup
-            progress = ((global_step - warmup_steps) / 
-                        (total_training_steps - warmup_steps))
-            lr = min_lr + (peak_lr - min_lr) * 0.5 * (1 + math.cos(math.pi * progress))
-    
-        # Apply the calculated learning rate to the optimizer
-        for param_group in optimizer.param_groups:
-            param_group["lr"] = lr
-        track_lrs.append(optimizer.param_groups[0]["lr"])
-
-            # Calculate and backpropagate the loss
-
-        loss = calc_loss_batch(input_batch, target_batch, model=dae, loss_fn=loss_fn, device='cpu')
-        loss.backward()
-
-        # Apply gradient clipping after the warmup phase to avoid exploding gradients
-
-        if global_step > warmup_steps:
-            torch.nn.utils.clip_grad_norm_(dae.parameters(), max_norm=1.0)  
-            
-        optimizer.step()
-        # Periodically evaluate the model on the training and validation sets
-        if global_step % eval_freq == 0:
-            train_loss, val_loss = evaluate_model(
-                dae, train_loader, test_loader,
-                'cpu', 1, loss_fn
-            )
-            train_losses.append(train_loss)
-            val_losses.append(val_loss)
-            # Print the current losses
-            print(f"Ep {epoch+1} (Iter {global_step:06d}): "
-                    f"Train loss {train_loss:.3f}, "
-                    f"Val loss {val_loss:.3f}"
-            )
-
+# n_epochs = 1000
+# dae = DAE(d=X_train_trf.shape[1])
+# optimizer = torch.optim.AdamW(dae.parameters(), weight_decay=0.1)
+# total_steps = len(train_loader) * n_epochs
+# warmup_steps = int(0.2 * total_steps) # 20% warmup
+# loss_fn = nn.MSELoss()
+# train_model(
+#     model=dae,
+#     train_loader=train_loader,
+#     test_loader=test_loader,
+#     optimizer=optimizer,
+#     device='cpu',
+#     n_epochs=n_epochs,
+#     eval_freq=500, 
+#     eval_iter=1,
+#     warmup_steps=20, 
+#     loss_fn=nn.MSELoss(),
+#     initial_lr=1e-5, min_lr=1e-5
+# )
 
 # # regression
+# # data for regression
 # dae.eval()
-# X_train_dae = torch.from_numpy(X_train_trf).float()
+# X_train_dae = torch.from_numpy(X_train_trf).double()
 # X_trf = dae.encode(X_train_dae).detach()
-# print(X_trf.shape)
-# reg = nn.Sequential(
-#     nn.Linear(X_trf.shape[1], 24),
-#     nn.ReLU(),
-#     nn.Linear(24, 12),
-#     nn.ReLU(),
-#     nn.Linear(12, 6),
-#     nn.ReLU(),
-#     nn.Linear(6, 1)
+y_train_trf = torch.from_numpy(y_train.to_numpy().reshape(-1, 1)).double().squeeze()
+reg_batch_size = 2**8
+reg_train_loader = DataLoader(
+    list(zip(X_train_trf, y_train_trf)),
+    batch_size=reg_batch_size,
+    shuffle=shuffle,
+    drop_last=drop_last,
+    num_workers=num_workers
+)
+reg_test_loader = DataLoader(
+    list(zip(X_test_trf, y_test)),
+    batch_size=reg_batch_size,
+    shuffle=shuffle,
+    drop_last=drop_last,
+    num_workers=num_workers
+)
+# print(X_trf.shape, y_train_trf.shape)
+
+reg = nn.Sequential(
+    nn.Linear(X_train_trf.shape[1], 2**12),
+    nn.ReLU(),
+    nn.Linear(2**12, 2**8),
+    nn.ReLU(),
+    nn.Linear(2**8, 2**4),
+    nn.ReLU(),
+    nn.Linear(2**4, 1),
+    # nn.ReLU(),
+    # nn.Linear(32, 16),
+    # nn.ReLU(),
+    # nn.Linear(16, 1),
+).double()
+print(y_train.shape)
+loss=nn.MSELoss() # loss function
+optimizer = torch.optim.AdamW(reg.parameters(), weight_decay=0)
+
+# training the model:
+train_model(
+    model=reg,
+    train_loader=reg_train_loader,
+    test_loader=reg_test_loader,
+    optimizer=optimizer,
+    device='cpu',
+    n_epochs=10,
+    eval_freq=500, 
+    eval_iter=1,
+    warmup_steps=1, 
+    loss_fn=nn.MSELoss(),
+    early_stopper=None,
+    initial_lr=1e-5, min_lr=1e-5
+)
+
+
+
+# test_loader = DataLoader(
+#     list(zip(torch.from_numpy(X_test.to_numpy()), torch.from_numpy(y_test.to_numpy()))),
+#     batch_size=batch_size,
+#     shuffle=shuffle,
+#     drop_last=drop_last,
+#     num_workers=num_workers
 # )
-# y_train_trf = torch.from_numpy(y_train.to_numpy().reshape(-1, 1)).float().squeeze()
-# print(y_train.shape)
-# loss=nn.MSELoss() # loss function
-# optimizers=optim.Adam(params=reg.parameters(),lr=0.01)
+# # 
+# score_predictions(preproc,dae,reg,X_test,y_test)
+
+# import xgboost as xgb
+
+# tree = xgb.XGBRegressor()
+# tree.fit(X_train_trf, y_train_trf)
 
 
-# # training the model:
-# num_of_epochs=100
-# for i in range(num_of_epochs):
-#   # give the input data to the architecure
-#   y_train_prediction=reg(X_trf)  # model initilizing
-#   loss_value=loss(y_train_prediction.squeeze(),y_train_trf)   # find the loss function:
-#   optimizers.zero_grad() # make gradients zero for every iteration so next iteration it will be clear
-#   loss_value.backward()  # back propagation
-#   optimizers.step()  # update weights in NN
+class MLP(nn.Module):
+    def __init__(self, in_dim, out_dim=1):
+        super(MLP, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(in_dim, 10000),
+            nn.ReLU(),
+            nn.Linear(10000, 1),
+            # nn.ReLU(),
+            # nn.Linear(32, 16),
+            # nn.ReLU(),
+            # nn.Linear(16, 1)
+        ).double()
+        
+    def forward(self, x):
+        return self.layers(x)
+num_epochs = 1000
+batch_size = 256
+loss_func = nn.MSELoss()
+mlp_torch = MLP(in_dim=X_train_trf.shape[1])
+optimizer = torch.optim.Adam(mlp_torch.parameters(), lr=0.01, weight_decay=0.01)
+trainloader = DataLoader(list(zip(X_train_trf, y_train)), batch_size=batch_size)
+testloader = DataLoader(list(zip(X_test_trf[:10000], y_test[:10000])), batch_size=batch_size)
 
-#   # print the loss in training part:
-#   if i % 10 == 0:
-#     print(f'[epoch:{i}]: The loss value for training part={loss_value}')
+### training step
+in_sample_r2_ = []
+for epoch in range(num_epochs): 
+    in_sample_r2_temp = []
+    running_loss = []
+    for id_batch, (X_batch, y_batch) in enumerate(trainloader):
+        optimizer.zero_grad()
+        y_pred = mlp_torch(X_batch)
+        loss = loss_func(y_pred, y_batch.unsqueeze(1))
+        loss.backward()
+        optimizer.step()
+        running_loss.append(loss.item())
+        # store in-sample R-squared
+        in_sample_r2_temp.append(r2_score(y_batch.detach().numpy(), y_pred.T[0].detach().numpy()))
+    in_sample_r2_.append(np.mean(in_sample_r2_temp))
+    
+    if epoch % 5 == 1:
+        print(f"Epoch {epoch}: {np.mean(running_loss)}, averaged in-sample R-squared is: {np.mean(in_sample_r2_temp)}")
